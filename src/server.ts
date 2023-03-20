294b8b8546c7ef6e630d389b1c092ce421479d20
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-20 19:01:34 -0300

    fix server

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
 import app from "./app";
-// import AppDataSource from "./data-source";
-// import "dotenv/config";
+import AppDataSource from "./data-source";
+import "dotenv/config";
 
-app.listen(3000, async () => {
-  console.log("Server running in port 3000");
-});
-
-// (async () => {
-//   await AppDataSource.initialize()
-//     .then(() => {
-//       console.warn(`Database connected!`);
-//       app.listen(process.env.PORT, () => {
-//         console.warn(`Server running in port ${process.env.PORT}`);
-//       });
-//     })
-//     .catch((err) => {
-//       console.error("Error during Data Source initialization", err);
-//     });
-// })();
+(async () => {
+  await AppDataSource.initialize()
+    .then(() => {
+      console.warn(`Database connected!`);
+      app.listen(process.env.PORT, () => {
+        console.warn(`Server running in port ${process.env.PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err);
+    });
+})();
