f8e1d7d1b3f9c64af55bbfac654c839ec7ed6008
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-20 19:11:28 -0300

    fix config data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,7 +9,7 @@ const dataSouceConfig = (): DataSourceOptions => {
     url: process.env.DATABASE_URL!,
     synchronize: false,
     logging: true,
-    entities: [path.join(__dirname, "./entities/**.{js,ts}")],
+    entities: [path.join(__dirname, "./models/**.{js,ts}")],
     migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
   };
 };
@@ -20,7 +20,7 @@ const AppDataSource = new DataSource(
         type: "sqlite",
         database: ":memory:",
         synchronize: true,
-        entities: ["src/entities/*.ts"],
+        entities: [path.join(__dirname, "./models/**.{js,ts}")],
       }
     : dataSouceConfig()
 );
