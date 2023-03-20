fc636973cf68853dc65535c8053db14b7ad23983
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-20 17:51:35 -0300

    feat interface user

---
+++ src/interfaces/users/index.ts
@@ -0,0 +1,12 @@
+export interface IUserReq {
+  name: string;
+  email: string;
+  phone: string;
+}
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  createdAt: Date;
+}
