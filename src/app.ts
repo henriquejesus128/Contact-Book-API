84eb1314b38ea4acd915c53d95e28af75232bbfc
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-20 20:13:17 -0300

    create route session

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import user from "./routers/users.routes";
+import session from "./routers/session.routes";
 import contact from "./routers/contacts.routes";
 import errorHandler from "./errors/handleError";
 
@@ -8,6 +9,7 @@ const app: Application = express();
 app.use(express.json());
 
 app.use("/users", user);
+app.use(`/session`, session);
 app.use("/contacts", contact);
 app.use(errorHandler);
 
