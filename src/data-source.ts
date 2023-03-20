ec2b1e3b10a6c854413065e517a61e77b8ab86be
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-20 19:01:00 -0300

    config data source postgres

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,27 +1,28 @@
-import { DataSource } from "typeorm"
-import path from "path"
-import "dotenv/config"
+import { DataSource, DataSourceOptions } from "typeorm";
+import path from "path";
+import "reflect-metadata";
+import "dotenv/config";
+
+const dataSouceConfig = (): DataSourceOptions => {
+  return {
+    type: "postgres",
+    url: process.env.DATABASE_URL!,
+    synchronize: false,
+    logging: true,
+    entities: [path.join(__dirname, "./entities/**.{js,ts}")],
+    migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
+  };
+};
 
 const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test" ?
-    {
+  process.env.NODE_ENV === "test"
+    ? {
         type: "sqlite",
         database: ":memory:",
         synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
-        type: "postgres",
-        host: process.env.PGHOST,
-        port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
-        synchronize: false,
-        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")]
-    }
-)
+        entities: ["src/entities/*.ts"],
+      }
+    : dataSouceConfig()
+);
 
-export default AppDataSource
+export default AppDataSource;
