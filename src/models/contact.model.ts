5dd07e0264432a19bbb35f3c787851508485bb05
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 10:11:31 -0300

    feat model contact

---
+++ src/models/contact.model.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
+
+@Entity("contacts")
+export class Contact {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 100, unique: true })
+  email: string;
+
+  @Column({ length: 20, unique: true })
+  phone: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
