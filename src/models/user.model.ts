0baecd384201c5bc3810e2a7d38e8989fdf45ef4
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 10:11:21 -0300

    feat model user

---
+++ src/models/user.model.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 100, unique: true })
+  email: string;
+
+  @Column({ length: 100 })
+  password: string;
+
+  @Column({ length: 20, unique: true })
+  phone: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
