4996842c02446fad71897f187db4093611b2f180
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 15:15:32 -0300

    fix schema user SchemaOf

--- src/schemas/user/user.schemas.ts
+++ src/schemas/user/user.schemas.ts
@@ -1,22 +1,22 @@
 import * as yup from "yup";
-import { ObjectSchema } from "yup";
+import { ObjectSchema, SchemaOf } from "yup";
 import { IUser, IUserReq, IUserUpdate } from "../../interfaces/users";
 
-export const userSchema: ObjectSchema<IUserReq> = yup.object().shape({
+export const userSchema: SchemaOf<IUserReq> = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
   password: yup.string().min(6).required(),
   phone: yup.string().min(20).required(),
 });
 
-export const userUpdate: ObjectSchema<IUserUpdate> = yup.object().shape({
+export const userUpdate: SchemaOf<IUserUpdate> = yup.object().shape({
   name: yup.string().notRequired(),
   email: yup.string().email().notRequired(),
   password: yup.string().min(6).notRequired(),
   phone: yup.string().min(20).notRequired(),
 });
 
-export const userReturned: ObjectSchema<IUser> = yup.object().shape({
+export const userReturned: SchemaOf<IUser> = yup.object().shape({
   id: yup.string().required(),
   name: yup.string().required(),
   email: yup.string().email().required(),
