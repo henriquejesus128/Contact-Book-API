bc8287f0f58aac0ee8b4e2efe3f41fafc009ad03
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 13:22:52 -0300

    fix types schemas

--- src/schemas/contact/contact.schemas.ts
+++ src/schemas/contact/contact.schemas.ts
@@ -1,24 +1,24 @@
 import * as yup from "yup";
-import { SchemaOf } from "yup";
+import { ObjectSchema } from "yup";
 import {
   IContact,
   IContactReq,
   IContactUpdate,
 } from "../../interfaces/contacts";
 
-export const contactSchema: SchemaOf<IContactReq> = yup.object().shape({
+export const contactSchema: ObjectSchema<IContactReq> = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
   phone: yup.string().min(20).required(),
 });
 
-export const contactUpdate: SchemaOf<IContactUpdate> = yup.object().shape({
+export const contactUpdate: ObjectSchema<IContactUpdate> = yup.object().shape({
   name: yup.string().notRequired(),
   email: yup.string().email().notRequired(),
   phone: yup.string().min(20).required(),
 });
 
-export const contactReturned: SchemaOf<IContact> = yup.object().shape({
+export const contactReturned: ObjectSchema<IContact> = yup.object().shape({
   id: yup.string().notRequired(),
   name: yup.string().notRequired(),
   email: yup.string().email().notRequired(),
--- src/schemas/user/user.schemas.ts
+++ src/schemas/user/user.schemas.ts
@@ -1,22 +1,22 @@
 import * as yup from "yup";
-import { SchemaOf } from "yup";
+import { ObjectSchema } from "yup";
 import { IUser, IUserReq, IUserUpdate } from "../../interfaces/users";
 
-export const userSchema: SchemaOf<IUserReq> = yup.object().shape({
+export const userSchema: ObjectSchema<IUserReq> = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
   password: yup.string().min(6).required(),
   phone: yup.string().min(20).required(),
 });
 
-export const userUpdate: SchemaOf<IUserUpdate> = yup.object().shape({
+export const userUpdate: ObjectSchema<IUserUpdate> = yup.object().shape({
   name: yup.string().notRequired(),
   email: yup.string().email().notRequired(),
   password: yup.string().min(6).required(),
   phone: yup.string().min(20).required(),
 });
 
-export const userReturned: SchemaOf<IUser> = yup.object().shape({
+export const userReturned: ObjectSchema<IUser> = yup.object().shape({
   id: yup.string().notRequired(),
   name: yup.string().notRequired(),
   email: yup.string().email().notRequired(),
