f68d27eda1ba59700406390758f2de196f1bfdc4
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 12:02:56 -0300

    fix route user

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import {
+  createUserController,
+  deleteUserController,
+  listUsersController,
+  retriveUserController,
+  updateUserController,
+} from "../controllers/user/users.controllers";
+import dataValidMiddleware from "../middleware/dataValid.middleware";
+import tokenAuthMiddleware from "../middleware/tokenAuth.middleware";
+import { userSchema, userUpdate } from "../schemas/user/user.schemas";
 
 const userRoutes: Router = Router();
 
-userRoutes.post("");
-userRoutes.get("");
-userRoutes.get("/:id");
-userRoutes.patch("/:id");
-userRoutes.delete("/:id");
+userRoutes.post("", dataValidMiddleware(userSchema), createUserController);
+userRoutes.get("", listUsersController);
+userRoutes.get("/:id", tokenAuthMiddleware, retriveUserController);
+userRoutes.patch(
+  "/:id",
+  dataValidMiddleware(userUpdate),
+  tokenAuthMiddleware,
+  updateUserController
+);
+userRoutes.delete("/:id", tokenAuthMiddleware, deleteUserController);
 
 export default userRoutes;
