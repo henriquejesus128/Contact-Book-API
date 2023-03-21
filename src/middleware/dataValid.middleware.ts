d1b143e337ff5c20b2699e6641e0f6f239bae043
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 11:31:58 -0300

    feat middleware data valid

---
+++ src/middleware/dataValid.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { AnySchema } from "yup";
+
+const dataValidMiddleware =
+  (schema: AnySchema) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const validated = await schema.validate(req.body, {
+        abortEarly: false,
+        stripUnknown: false,
+      });
+      req.body = validated;
+      return next();
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  };
+export default dataValidMiddleware;
