3b9a4b4b2c50d106802ee7e9cf12a05775996f9c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 12:32:39 -0300

    feat schema contact

---
+++ src/schemas/contact/contact.schemas.ts
@@ -0,0 +1,28 @@
+import * as yup from "yup";
+import { SchemaOf } from "yup";
+import {
+  IContact,
+  IContactReq,
+  IContactUpdate,
+} from "../../interfaces/contacts";
+
+export const contactSchema: SchemaOf<IContactReq> = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  phone: yup.string().min(20).required(),
+});
+
+export const contactUpdate: SchemaOf<IContactUpdate> = yup.object().shape({
+  name: yup.string().notRequired(),
+  email: yup.string().email().notRequired(),
+  phone: yup.string().min(20).required(),
+});
+
+export const contactReturned: SchemaOf<IContact> = yup.object().shape({
+  id: yup.string().notRequired(),
+  name: yup.string().notRequired(),
+  email: yup.string().email().notRequired(),
+  phone: yup.string().notRequired(),
+  createdAt: yup.date().notRequired(),
+  updatedAt: yup.date().notRequired(),
+});
