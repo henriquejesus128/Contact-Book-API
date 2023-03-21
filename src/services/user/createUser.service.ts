be3ffa878b97ad932b32ea69ee717ff84ed6ecb4
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 11:06:28 -0300

    fix create user

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,4 +1,24 @@
 import { IUser, IUserReq } from "../../interfaces/users";
+import AppDataSource from "../../data-source";
+import { AppError } from "../../errors/AppError";
 
-const createUserService = async (body: IUserReq): Promise<IUser> => {};
+const createUserService = async (body: IUserReq): Promise<IUser> => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const { email } = body;
+
+  const foundUser = await userRepository.findOneBy({ email: email });
+
+  if (foundUser) {
+    throw new AppError(`User already exists!`, 409);
+  }
+
+  const user = userRepository.create(body);
+
+  await userRepository.save(user);
+
+  const newUser = await userReturned.validate(user, { stripUnknown: true });
+
+  return newUser;
+};
 export default createUserService;
