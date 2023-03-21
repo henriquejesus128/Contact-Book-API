e36bd18ef789b9c1afb90dda8a47fda4b95a486c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 11:13:57 -0300

    fix delete user

--- src/services/user/deleteUser.service.ts
+++ src/services/user/deleteUser.service.ts
@@ -1,2 +1,21 @@
-const deleteUserService = async (id: string) => {};
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/AppError";
+
+const deleteUserService = async (id: string) => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const user = await userRepository.findOneBy({ id });
+
+  const { isActive } = user;
+
+  if (!isActive) {
+    throw new AppError("Disabled user!", 400);
+  }
+
+  user.isActive = false;
+  await userRepository.save(user);
+
+  return {};
+};
 export default deleteUserService;
