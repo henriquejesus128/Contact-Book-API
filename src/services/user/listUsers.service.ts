7627420dad2fbd8bb42913ff74f588d1cf19a21d
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-21 11:14:05 -0300

    fix list users

--- src/services/user/listUsers.service.ts
+++ src/services/user/listUsers.service.ts
@@ -1,2 +1,17 @@
-const listUsersService = async () => {};
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+import { IUser } from "../../interfaces/users";
+import { listUsers } from "../../schemas/user/user.schemas";
+
+const listUsersService = async (): Promise<IUser[]> => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const users = await userRepository.find();
+
+  const newListUsers = await listUsers.validate(users, {
+    stripUnknown: true,
+  });
+
+  return newListUsers;
+};
 export default listUsersService;
