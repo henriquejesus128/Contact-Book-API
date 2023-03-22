ff9b53e1f0ea10b83df84c056355ec24a48fee6e
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 12:40:07 -0300

    fix list contacts user

--- src/services/contact/listContacts.service.ts
+++ src/services/contact/listContacts.service.ts
@@ -1,12 +1,16 @@
 import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
 import { IContact } from "../../interfaces/contacts";
+import { ITokenUser } from "../../interfaces/users";
 import { listContacts } from "../../schemas/contact/contact.schemas";
 
-const listContactsService = async (): Promise<IContact[]> => {
+const listContactsService = async (user: ITokenUser): Promise<IContact[]> => {
   const contactRepository = AppDataSource.getRepository(Contact);
 
-  const contacts = await contactRepository.find();
+  const contacts = await contactRepository.findOne({
+    where: { id: user.id },
+    relations: { user: true },
+  });
 
   const validListContact = await listContacts.validate(contacts, {
     stripUnknown: true,
