e49ef5f7cd2c861a2da665b6e6340bb6e2794000
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 20:11:24 -0300

    fix clear code

--- src/middleware/existUser.middleware.ts
+++ src/middleware/existUser.middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import AppDataSource from "../data-source";
 import { User } from "../entities/user.entity";
-import { AppError } from "../errors/AppError";
 
 const userExistsMiddleware = async (
   req: Request,
