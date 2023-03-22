5a25720b883267d048d10bbef79d47dc05042076
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 16:20:37 -0300

    fix route user

--- src/routers/session.routes.ts
+++ src/routers/session.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { sessionController } from "../controllers/session/session.controller";
+import dataValidMiddleware from "../middleware/dataValid.middleware";
+import { sessionSchema } from "../schemas/session/session.schemas";
 
 const sessionRouter = Router();
 
-sessionRouter.post("", sessionController);
+sessionRouter.post("", dataValidMiddleware(sessionSchema), sessionController);
 
 export default sessionRouter;
