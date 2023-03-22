c6b8c62be2d8d6758d5b15977a2d4f213300d8c1
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 11:25:22 -0300

    fix route contact

--- src/routers/contacts.routes.ts
+++ src/routers/contacts.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import {
+  createContactController,
+  deleteContactController,
+  listContactsController,
+  retriveContactController,
+  updateContactController,
+} from "../controllers/contact/contacts.controller";
 import tokenAuthMiddleware from "../middleware/tokenAuth.middleware";
 
 const contactRoutes: Router = Router();
 
-contactRoutes.post("", tokenAuthMiddleware);
-contactRoutes.get("", tokenAuthMiddleware);
-contactRoutes.get("/:id", tokenAuthMiddleware);
-contactRoutes.patch("/:id", tokenAuthMiddleware);
-contactRoutes.delete("/:id", tokenAuthMiddleware);
+contactRoutes.post("", tokenAuthMiddleware, createContactController);
+contactRoutes.get("", tokenAuthMiddleware, listContactsController);
+contactRoutes.get("/:id", tokenAuthMiddleware, retriveContactController);
+contactRoutes.patch("/:id", tokenAuthMiddleware, updateContactController);
+contactRoutes.delete("/:id", tokenAuthMiddleware, deleteContactController);
 
 export default contactRoutes;
