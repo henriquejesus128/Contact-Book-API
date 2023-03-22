0a34e82eaf072aac94294d64810e61bdc4ce0023
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 15:44:36 -0300

    fix errorHandler

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application } from "express";
 import user from "./routers/users.routes";
 import session from "./routers/session.routes";
 import contact from "./routers/contacts.routes";
-import errorHandler from "./errors/handleError";
+import errorHandler from "./errors/errorHandler";
 
 const app: Application = express();
 
