2584ef469108bb0bd5cccdda0a9af9615c155312
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 09:18:06 -0300

    fix migrate

--- src/migrations/1679486670654-InitialMigraion.ts
+++ src/migrations/1679486670654-InitialMigraion.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigraion1679430079557 implements MigrationInterface {
-    name = 'InitialMigraion1679430079557'
+export class InitialMigraion1679486670654 implements MigrationInterface {
+    name = 'InitialMigraion1679486670654'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "email" character varying(100) NOT NULL, "password" character varying(100) NOT NULL, "phone" character varying(20) NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_a000cca60bcf04454e727699490" UNIQUE ("phone"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
