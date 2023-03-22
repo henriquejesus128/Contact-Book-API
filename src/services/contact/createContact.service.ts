5cd098a4dd85f2a6e5e811b0cdbf8792a339f4aa
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 10:53:34 -0300

    fix create contact

--- src/services/contact/createContact.service.ts
+++ src/services/contact/createContact.service.ts
@@ -1,12 +1,19 @@
 import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
+import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors/AppError";
 import { IContact, IContactReq } from "../../interfaces/contacts";
+import { ITokenUser } from "../../interfaces/users";
 import { contactReturned } from "../../schemas/contact/contact.schemas";
 
-const createContactService = async (body: IContactReq): Promise<IContact> => {
+const createContactService = async (
+  body: IContactReq,
+  user: ITokenUser
+): Promise<IContact> => {
   const contactRepository = AppDataSource.getRepository(Contact);
+  const userRepository = AppDataSource.getRepository(User);
   const { email } = body;
+  const { id } = user;
 
   const foundContact = await contactRepository.findOneBy({ email });
 
@@ -14,7 +21,11 @@ const createContactService = async (body: IContactReq): Promise<IContact> => {
     throw new AppError(`Contact exists!`, 409);
   }
 
+  const foundUser = await userRepository.findOneBy({ id });
+
   const contact = contactRepository.create(body);
+
+  contact.user = foundUser;
 
   await contactRepository.save(contact);
 
