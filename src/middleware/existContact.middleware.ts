bda370dcdc987923e505c84f961f9713ac5baf08
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 19:17:50 -0300

    fix middleware contact exist

--- src/middleware/existContact.middleware.ts
+++ src/middleware/existContact.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { string } from "pg-format";
 import AppDataSource from "../data-source";
 import { Contact } from "../entities/contact.entity";
+import { User } from "../entities/user.entity";
 
 const existContactMiddleware = async (
   req: Request,
@@ -9,13 +10,16 @@ const existContactMiddleware = async (
   next: NextFunction
 ) => {
   const contactRepository = AppDataSource.getRepository(Contact);
+
   const { id } = req.user;
   const { email } = req.body;
-  console.log(id);
-  const findContact = await contactRepository.findOneBy({ email: email });
-  console.log(findContact.user);
 
-  if (findContact) {
+  const findContact = await contactRepository.findOne({
+    where: { email: email },
+    relations: { user: true },
+  });
+
+  if (findContact.user.id == id) {
     return res.status(409).json({ message: "Contact exists!" });
   }
 
