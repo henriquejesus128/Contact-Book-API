4bf4a3497f0ad190d3a3120e62fe2785d8d4030d
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-22 16:24:09 -0300

    fix data valid middleware

--- src/middleware/dataValid.middleware.ts
+++ src/middleware/dataValid.middleware.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { AnySchema } from "yup";
 
 const dataValidMiddleware =
-  (schema: AnySchema) => async (req: Request, res: Response, next: NextFunction) => {
+  (schema: AnySchema) =>
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const validated = await schema.validate(req.body, {
         abortEarly: false,
@@ -11,7 +12,7 @@ const dataValidMiddleware =
       req.body = validated;
       return next();
     } catch (err) {
-      return res.status(400).json({ message: err });
+      return res.status(400).json({ message: err.message });
     }
   };
 export default dataValidMiddleware;
