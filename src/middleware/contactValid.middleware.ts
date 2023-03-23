bdc083239480dac7c033fa0a435a974d8aec96b9
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-23 09:22:40 -0300

    fix middleware valid contact

--- src/middleware/contactValid.middleware.ts
+++ src/middleware/contactValid.middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import AppDataSource from "../data-source";
 import { Contact } from "../entities/contact.entity";
-import { User } from "../entities/user.entity";
 
 const contactValidMiddleware = async (
   req: Request,
@@ -9,27 +8,18 @@ const contactValidMiddleware = async (
   next: NextFunction
 ) => {
   const contactRepository = AppDataSource.getRepository(Contact);
-  const userRepository = AppDataSource.getRepository(User);
 
   const { id } = req.user;
   const { email } = req.body;
 
-  const user = await userRepository.findOneBy({ id: id });
+  const findContact = await contactRepository.findOne({
+    where: { email: email },
+    relations: { user: true },
+  });
 
-  // const findContact = await contactRepository.findOne({
-  //   where: { email: email },
-  //   relations: { user: true },
-  // });
-
-  // if (findContact.user.id == id) {
-  //   return res.status(409).json({ message: "Contact exists!" });
-  // }
-
-  // const findContact = await contactRepository.findOneBy({ email: email });
-
-  // if (findContact) {
-  //   return res.status(409).json({ message: "Contact exists!" });
-  // }
+  if (findContact.user.id == id) {
+    return res.status(409).json({ message: "Contact exists!" });
+  }
 
   return next();
 };
