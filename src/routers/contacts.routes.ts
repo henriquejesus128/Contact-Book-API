6aec6cd466d3757ee73f61a6ed6230c4c694c3f0
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-23 15:03:39 -0300

    fix routes contact

--- src/routers/contacts.routes.ts
+++ src/routers/contacts.routes.ts
@@ -8,7 +8,7 @@ import {
 } from "../controllers/contact/contacts.controller";
 import dataValidMiddleware from "../middleware/dataValid.middleware";
 import tokenAuthMiddleware from "../middleware/tokenAuth.middleware";
-// import contactValidMiddleware from "../middleware/contactValid.middleware";
+import contactValidMiddleware from "../middleware/contactValid.middleware";
 import existContactMiddleware from "../middleware/existContact.middleware";
 import { contactSchema } from "../schemas/contact/contact.schemas";
 
@@ -18,7 +18,7 @@ contactRoutes.post(
   "",
   tokenAuthMiddleware,
   dataValidMiddleware(contactSchema),
-  // contactValidMiddleware,
+  contactValidMiddleware,
   createContactController
 );
 contactRoutes.get("", tokenAuthMiddleware, listContactsController);
