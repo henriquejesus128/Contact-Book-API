7f8c48850da0b041b067bb8eb8e8efd41bd5584e
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 00:53:49 -0300

    fix app cors

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import user from "./routers/users.routes";
 import session from "./routers/session.routes";
 import contact from "./routers/contacts.routes";
 import errorHandler from "./errors/errorHandler";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", user);
 app.use(`/session`, session);
